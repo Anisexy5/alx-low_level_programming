e7c0e3cfd42e83bfae2ef01fe790652c581c284c
Anisexy5 <emmanuelanita38@gmail.com>
2023-06-29 10:38:14 -0700

    file that arranges

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,28 +1,28 @@
 #include "main.h"
 /**
- * _strcat - concatenates two strings
- * @dest: input value
- * @src: input value
- *
- * Return: void
+* _strcat - concatenates two strings
+* @dest: input value
+* @src: input value
+*
+* Return: void
  **/
 char *_strcat(char *dest, char *src)
 {
+	int a;
 	int b;
-	int c;
 
+	a = 0;
+	while (dest[a] != '\0')
+	{
+		a++;
+	}
 	b = 0;
-	while (dest[b] != '\0')
+	while (src[b] != '\0')
 	{
+		dest[a] = src[b];
+		a++;
 		b++;
 	}
-	c = 0;
-	while (src[c] != '\0')
-	{
-		dest[b] = src[c];
-		b++;
-		c++;
-	}
-	dest[i] = '\0';
+	dest[a] = '\0';
 	return (dest);
 }
